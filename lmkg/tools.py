b04a2cdfbb59af1d691ae56a6f2287d160228941
Daniel <dfdazac@gmail.com>
2024-08-09 15:05:05 +0200

    First version of LMKG tools

--- lmkg/tools.py
+++ lmkg/tools.py
@@ -1,21 +1,34 @@
 from datetime import datetime
 
 
-def current_time():
-    """Get the current local time as a string."""
-    return str(datetime.now())
+def get_definition(unique_id: str):
+    """Retrieve textual definition of an entity or predicate given its unique identifier."""
+    pass
+
+def get_most_similar(unique_id: str):
+    """Retrieve a list of entities or predicates that are semantically similar to a given entity or predicate identifier."""
+    pass
+
+def search_predicate(query: str):
+    """Search for predicates that match the given query."""
+    pass
+
+def get_predicates_of_subject(entity_id: str):
+    """Get a random list of predicates in which the given entity occurs as a subject."""
+    pass
+
+def get_predicates_of_object(entity_id: str):
+    """Get a random list of predicates in which the given entity occurs as an object."""
+    pass
+
+def get_head_entities(predicate_id: str):
+    """Get a random list of predicates in which the given entity occurs as a subject."""
+    pass
+
+def get_tail_entities(predicate_id: str):
+    """Get a random list of predicates in which the given entity occurs as an object."""
+    pass
 
 
-def multiply(a: float, b: float):
-    """
-    A function that multiplies two numbers
 
-    Args:
-        a: The first number to multiply
-        b: The second number to multiply
-    """
-    return a * b
-
-
-tools_dict = {"current_time": current_time,
-         "multiply": multiply}
+tools_dict = {}
